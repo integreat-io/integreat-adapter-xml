f25f49b099c5a08b1e38261eb63c8b7faed3f828
Kjell-Morten <post@kjellmorten.no>
2023-05-16 19:54:13 +0200

    Refactor

--- src/utils/setNamespaceAttrs.ts
+++ src/utils/setNamespaceAttrs.ts
@@ -32,58 +32,94 @@ const setChildrenOrParent = (
     parents
   )
 
-const getPrefixesFromElement =
-  (prefixes: string[], parent: ObjectElement) =>
-  ([key, data]: [string, ObjectElement | ObjectElement[]]): PrefixParents => {
-    if (Array.isArray(data)) {
-      return data
-        .map((element) => getPrefixParents(element, prefixes))
-        .reduce(
-          (prefixParents, prefixChildren) =>
-            setChildrenOrParent(prefixParents, prefixChildren, parent),
-          {}
-        )
-    }
-
-    const childNodes = Object.entries(data)
-    const localPrefixes = [
-      extractPrefix(key),
-      ...childNodes
-        .filter(isAttribute)
-        .map(([key]) => extractPrefix(key.slice(1)))
-        .filter(Boolean),
-    ]
-    const localPrefixParents = localPrefixes.reduce(
-      (parents, prefix) => ({ ...parents, [prefix]: data }),
-      {} as PrefixParents
-    )
-    const nextPrefixes = prefixes.filter((pre) => !localPrefixes.includes(pre))
-    if (nextPrefixes.length === 0) {
-      return localPrefixParents // Stop when there are no more prefixes to find
-    }
-    const parents = childNodes
-      .filter(isNode)
-      .map(getPrefixesFromElement(nextPrefixes, data))
-    return parents.reduce(
-      (prefixParents, prefixChildren) =>
-        setChildrenOrParent(prefixParents, prefixChildren, data),
-      localPrefixParents
-    )
-  }
-
-function getPrefixParents(data: ObjectElement, prefixes: string[]) {
-  const children = Object.entries(data).filter(isNode)
-  return children
-    .map((child) => getPrefixesFromElement(prefixes, data)(child))
+function getPrefixesFromArray(
+  data: ObjectElement[],
+  prefixes: string[],
+  parent: ObjectElement
+) {
+  return data
+    .map((element) => getPrefixParents(element, prefixes))
     .reduce(
-      (prefixParents, prefixChild) => ({
-        ...prefixChild,
-        ...prefixParents,
-      }),
+      (prefixParents, prefixChildren) =>
+        setChildrenOrParent(prefixParents, prefixChildren, parent),
       {}
     )
 }
 
+const findLocalPrefixes = (
+  key: string,
+  nodes: [string, string | ElementValue][]
+) => [
+  extractPrefix(key),
+  ...nodes
+    .filter(isAttribute)
+    .map(([key]) => extractPrefix(key.slice(1)))
+    .filter(Boolean),
+]
+
+const findPrefixParents = (
+  prefixes: string[],
+  data: ObjectElement<ElementValue>
+) =>
+  prefixes.reduce(
+    (parents, prefix) => ({ ...parents, [prefix]: data }),
+    {} as PrefixParents
+  )
+
+const findParents = (
+  nodes: [string, string | ElementValue][],
+  prefixes: string[],
+  data: ObjectElement<ElementValue>
+) => nodes.filter(isNode).map(getPrefixesFromElement(prefixes, data))
+
+const setChildOrParentOnPrefix = (
+  parents: PrefixParents[],
+  data: ObjectElement<ElementValue>,
+  localPrefixParents: PrefixParents
+) =>
+  parents.reduce(
+    (prefixParents, prefixChildren) =>
+      setChildrenOrParent(prefixParents, prefixChildren, data),
+    localPrefixParents
+  )
+
+// Find the relevant namespaces for an element
+const getPrefixesFromElement =
+  (prefixes: string[], parent: ObjectElement) =>
+  ([key, data]: [string, ObjectElement | ObjectElement[]]): PrefixParents => {
+    if (Array.isArray(data)) {
+      return getPrefixesFromArray(data, prefixes, parent)
+    }
+
+    const childNodes = Object.entries(data)
+    const localPrefixes = findLocalPrefixes(key, childNodes)
+    const localPrefixParents = findPrefixParents(localPrefixes, data)
+    const nextPrefixes = prefixes.filter((pre) => !localPrefixes.includes(pre))
+    if (nextPrefixes.length === 0) {
+      return localPrefixParents // Stop when there are no more prefixes to find
+    }
+    const parents = findParents(childNodes, nextPrefixes, data)
+    return setChildOrParentOnPrefix(parents, data, localPrefixParents)
+  }
+
+// Walk the data structure and return an object with namespace prefixes as keys
+// and the element where we need to set the define the namespace as value. This
+// may be the first element that uses a namespace, or the parent of two or more
+// elements that uses it. Our goal is to define the namespace as close to where
+// it is used as possible, but never more than once.
+function getPrefixParents(data: ObjectElement, prefixes: string[]) {
+  const children = Object.entries(data).filter(isNode)
+  return children.map(getPrefixesFromElement(prefixes, data)).reduce(
+    (prefixParents, prefixChild) => ({
+      ...prefixChild,
+      ...prefixParents,
+    }),
+    {}
+  )
+}
+
+// Go through the elements that we identified as the signle parent closest to
+// where a namespace is used, and set the appropriate xmlns attributes.
 function setAttrs(prefixParents: PrefixParents, namespaces: Namespaces) {
   Object.entries(prefixParents).forEach(([prefix, element]) => {
     // eslint-disable-next-line security/detect-object-injection
@@ -96,9 +132,11 @@ function setAttrs(prefixParents: PrefixParents, namespaces: Namespaces) {
   })
 }
 
+// Format dates as ISO dates and force anything else to string
 const formatValue = (value: unknown) =>
   isDate(value) ? value.toISOString() : String(value)
 
+// Iterates an array and makes its leaves ready for stringification.
 function fixLeavesInArray(key: string, value: unknown[], xsiNs: string) {
   if (value.length === 1) {
     return fixLeavesInValue(key, value[0], xsiNs)
@@ -108,6 +146,8 @@ function fixLeavesInArray(key: string, value: unknown[], xsiNs: string) {
     | ElementValue
 }
 
+// Sets nil attr for `null` values, formats attributes and leaf values, and
+// traverses anything else.
 function fixLeavesInValue(
   key: string,
   value: unknown,
@@ -128,6 +168,8 @@ function fixLeavesInValue(
   }
 }
 
+// Iterate the keys of an element and make sure the leaves are ready for being
+// stringified. When a key is not a leaf, it traverses down to the leaves.
 function fixLeavesInElement(data: ObjectElement<unknown>, xsiNs: string) {
   const element: ObjectElement = {}
   for (const [key, value] of Object.entries(data)) {
@@ -139,6 +181,7 @@ function fixLeavesInElement(data: ObjectElement<unknown>, xsiNs: string) {
   return element
 }
 
+// Run through data structure and set namespace attrs
 export default function setNamespaceAttrs(
   data: ObjectElement<unknown>,
   namespaces: Namespaces,
